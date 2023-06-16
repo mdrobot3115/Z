f73dfaa9748a72cb57af41ab43c6612eceb35372
mdrobot3115 <116418622+mdrobot3115@users.noreply.github.com>
2023-06-16 19:15:34 +0530

    Update ytdlp.py

--- bot/modules/ytdlp.py
+++ bot/modules/ytdlp.py
@@ -333,7 +333,7 @@ async def _ytdl(client, message, isZip=False, isLeech=False, sameDir=None, bulk=
     async def __run_multi():
         if multi <= 1:
             return
-        await sleep(45)
+        await sleep(35)
         ymsg = mssg.split(maxsplit=index)
         ymsg[mi] = f'{multi - 1}'
         if len(bulk) != 0:
